0a1d3788a5d3e52fa89a72e91e299513d60d8b85
AlexandraArseni <alexarseni@outlook.com>
2023-08-23 07:51:10 +0000

    Minor changes

--- src/virtualkey/main/VirtualKeyApp.java
+++ src/virtualkey/main/VirtualKeyApp.java
@@ -3,11 +3,18 @@ package virtualkey.main;
 import virtualkey.util.*;
 
 public class VirtualKeyApp {
+	
+	public static void welcome() {
+		System.out.println("Welcome to my application");
+		System.out.println("Application name: VirtualKeyApp");
+		System.out.println("Developer: Arseni Alexandra");
+		System.out.println("Email: alexandra.arseni@vodafone.com\n");
+	}
 
 	public static void main(String[] args) {
 		//Directory.makeDirectory(); //created the directory that will store our files
 		
-		WelcomeScreen.welcome(); //prints the Welcome Screen
+		welcome(); //prints the Welcome Screen
 		MainMenu.mainMenu();	 //calls the main menu method
 		
 		System.out.println("Bye Bye");	 
--- src/virtualkey/service/VirtualKeyService.java
+++ src/virtualkey/service/VirtualKeyService.java
@@ -15,16 +15,16 @@ public class VirtualKeyService {
 	}
 	//We use this function to create some test files
 	public void createSomeFiles() {
-		File file1 = new File(destination+"/file1.txt");
-		File file2 = new File(destination+"/file2.txt");
-		File file3 = new File(destination+"/maria.txt");
-		File file4 = new File(destination+"/alex.txt");
+		//File file1 = new File(destination+"/file1.txt");
+		//File file2 = new File(destination+"/file2.txt");
+		File file1 = new File(destination+"/maria.txt");
+		File file2 = new File(destination+"/alex.txt");
 		
 		try {
 			file1.createNewFile();
 			file2.createNewFile();
-			file3.createNewFile();
-			file4.createNewFile();
+			//file3.createNewFile();
+			//file4.createNewFile();
 		}
 		catch (IOException e){
 			System.out.println("Files could not be created - IO Exception");
@@ -38,23 +38,14 @@ public class VirtualKeyService {
 	//This method will display the contents of the directory
 	public String[] getFiles() {
 		
+		
 		String[] sortedFiles = destination.list(); //get the list of file names in the destination directory
 		//String[] sortedFiles = {"maria", "alex", "dimitra","danil"}; //astring array to check the sorting
-		
 		
 		if(sortedFiles.length!=0) {
 			Arrays.sort(sortedFiles); //sort the names 
 		}
 			return sortedFiles;
-//			for(String s:sortedFiles) { //iterate through the strings in the array string	
-//				System.out.println(s);
-//			}
-		
-//		else {
-//			String empty[] = {};
-//			return empty;
-//		}
-		
 	}
 	
 	public String addFile(String filename){
